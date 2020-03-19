f77f272e33e1fa393a536b0087902aa9e8fb149f
ahcdw <xing.wang@qiaofang.com>
2020-03-19 11:18:19 +0800

    70. Climbing Stairs

---
+++ src/com/wx/dp/Solution70.java
@@ -0,0 +1,46 @@
+package com.wx.dp;
+
+//********************************************************************************************
+//        70. Climbing Stairs
+//        Easy
+//
+//        You are climbing a stair case. It takes n steps to reach to the top.
+//
+//        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//
+//        Note: Given n will be a positive integer.
+//
+//        Example 1:
+//
+//        Input: 2
+//        Output: 2
+//        Explanation: There are two ways to climb to the top.
+//        1. 1 step + 1 step
+//        2. 2 steps
+//
+//        Example 2:
+//
+//        Input: 3
+//        Output: 3
+//        Explanation: There are three ways to climb to the top.
+//        1. 1 step + 1 step + 1 step
+//        2. 1 step + 2 steps
+//        3. 2 steps + 1 step
+//找到某一个状态，比如走到第i层，第i层上来要么是走一步，要么是走两步，因而是这两钟情况之和
+//********************************************************************************************
+public class Solution70 {
+    public static int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+
+    }
+}

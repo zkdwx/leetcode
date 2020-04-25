1edc21d99f6485738a422368849446334d17007a
ahcdw <xing.wang@qiaofang.com>
2020-04-25 19:11:20 +0800

    657. Robot Return to Origin

--- src/com/wx/test/Test.java
+++ src/com/wx/test/Test.java
@@ -1,33 +1,15 @@
 package com.wx.test;
 
-public class Test {
-    public int findSimilar(String a, String b) {
-        int aLen = a.length();
-        int bLen = b.length();
-        int[][] dp = new int[aLen + 1][bLen + 1];
-        for (int i = 0; i < aLen; i++) {
-            dp[i][0] = i;
-        }
-        for (int i = 0; i < bLen; i++) {
-            dp[0][i] = i;
-        }
-        for (int i = 1; i <= aLen; i++) {
-            for (int j = 1; j <= bLen; j++) {
-                if (a.charAt(i) == b.charAt(j)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-            }
-        }
-        return dp[aLen][bLen] / 2;
-    }
+import java.util.Scanner;
 
-    private void move(char[] chars, int i, int j) {
-        char temp = chars[i];
-        for (int k = i; k < j; k++) {
-            chars[i] = chars[i + 1];
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s= sc.nextLine();
+        String[] arr=s.split(" ");
+        int[] a=new int[arr.length];
+        for (int i = 0; i < a.length; i++) {
+            a[i]=Integer.parseInt(arr[i]);
         }
-        chars[j] = temp;
     }
 }
